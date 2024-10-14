9a682f342278c860b111697819bbc4c33984d12b
kweyaanton <kweyaanton@gmail.com>
2024-10-14 15:50:09 +0300

    adding a security branch

--- src/main/java/com/securityGear/app/Controllers/LoginController.java
+++ src/main/java/com/securityGear/app/Controllers/LoginController.java
@@ -5,7 +5,7 @@ import com.securityGear.app.Entities.User;
 import com.securityGear.app.Services.UserService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
